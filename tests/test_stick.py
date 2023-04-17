049d44c7746c2ca4bb2db3d7016a76dd95171109
Tom Scholten <git@scholten.nu>
2023-04-17 22:20:03 +0200

    Add black and test-class from initial, version bump high

--- tests/test_stick.py
+++ tests/test_stick.py
@@ -7,5 +7,11 @@ pw_constants = importlib.import_module("plugwise_usb.constants")
 pw_exceptions = importlib.import_module("plugwise_usb.exceptions")
 pw_stick = importlib.import_module("plugwise_usb")
 
+
 # No tests available
-assert True
+class TestPlugwise:  # pylint: disable=attribute-defined-outside-init
+    """Tests for Plugwise USB."""
+
+    async def test_connect_legacy_anna(self):
+        """No tests available."""
+        assert True
