b90e7fb70028d2a9d862b8f6421bdbe32a24bb52
Marc Dirix <marc@dirix.nu>
2024-01-03 20:17:24 +0100

    python format

--- plugwise_usb/nodes/circle.py
+++ plugwise_usb/nodes/circle.py
@@ -419,7 +419,7 @@ class PlugwiseCircle(PlugwiseNode):
                 )
             else:
                 _mem_address = self._energy_timestamp_memory_address(_log_timestamp)
-                if self._energy_history_failed_address.count(_mem_address) == 0
+                if self._energy_history_failed_address.count(_mem_address) == 0:
                     self._energy_history_failed_address.append(_mem_address)
                 _LOGGER.info(
                     "_collect_energy_pulses for %s at %s not found, request counter from memory %s (from mem=%s, slot=%s, timestamp=%s)",
