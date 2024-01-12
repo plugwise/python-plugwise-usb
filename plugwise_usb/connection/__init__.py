cc4e95562adf46aecfed38c33cf09a1375419fe7
Breugel <frank_van_breugel@hotmail.com>
2024-01-12 17:00:32 +0100

    Full rewrite to async version - known to be not working !
    Untested

---
+++ plugwise_usb/connection/__init__.py
@@ -0,0 +1,186 @@
+"""
+The 'Connection ' manage the connection and communication
+flow through the USB-Stick.
+"""
+from __future__ import annotations
+
+from collections.abc import Awaitable, Callable
+from concurrent import futures
+import logging
+
+from ..api import StickEvent
+from ..exceptions import StickError
+from ..messages.requests import PlugwiseRequest, StickInitRequest
+from ..messages.responses import PlugwiseResponse, StickInitResponse
+from .manager import StickConnectionManager
+from .queue import StickQueue
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class StickController():
+    """Manage the connection and communication towards USB-Stick."""
+
+    def __init__(self) -> None:
+        """Initialize Stick controller."""
+        self._manager = StickConnectionManager()
+        self._queue = StickQueue()
+        self._unsubscribe_stick_event = (
+            self._manager.subscribe_to_stick_events(
+                self._handle_stick_event, None
+            )
+        )
+
+        self._init_sequence_id: bytes | None = None
+        self._init_future: futures.Future | None = None
+
+        self._is_initialized = False
+        self._mac_stick: str | None = None
+        self._mac_nc: str | None = None
+        self._network_id: int | None = None
+        self._network_online = False
+
+    @property
+    def is_initialized(self) -> bool:
+        """Returns True if UBS-Stick connection is active and initialized."""
+        if not self._manager.is_connected:
+            return False
+        return self._is_initialized
+
+    @property
+    def is_connected(self) -> bool:
+        """Return connection state from connection manager"""
+        return self._manager.is_connected
+
+    @property
+    def mac_stick(self) -> str:
+        """
+        Returns the MAC address of USB-Stick.
+        Raises StickError when not connected.
+        """
+        if not self._manager.is_connected or self._mac_stick is None:
+            raise StickError(
+                "No mac address available. " +
+                "Connect and initialize USB-Stick first."
+            )
+        return self._mac_stick
+
+    @property
+    def mac_coordinator(self) -> str:
+        """
+        Return MAC address of the Zigbee network coordinator (Circle+).
+        Raises StickError when not connected.
+        """
+        if not self._manager.is_connected or self._mac_nc is None:
+            raise StickError(
+                "No mac address available. " +
+                "Connect and initialize USB-Stick first."
+            )
+        return self._mac_nc
+
+    @property
+    def network_id(self) -> int:
+        """
+        Returns the Zigbee network ID.
+        Raises StickError when not connected.
+        """
+        if not self._manager.is_connected or self._network_id is None:
+            raise StickError(
+                "No network ID available. " +
+                "Connect and initialize USB-Stick first."
+            )
+        return self._network_id
+
+    @property
+    def network_online(self) -> bool:
+        """Return the network state."""
+        if not self._manager.is_connected:
+            raise StickError(
+                "Network status not available. " +
+                "Connect and initialize USB-Stick first."
+            )
+        return self._network_online
+
+    async def connect_to_stick(self, serial_path: str) -> None:
+        """Setup connection to USB stick."""
+        await self._manager.setup_connection_to_stick(serial_path)
+
+    def subscribe_to_stick_events(
+        self,
+        stick_event_callback: Callable[[StickEvent], Awaitable[None]],
+        events: tuple[StickEvent],
+    ) -> Callable[[], None]:
+        """
+        Subscribe callback when specified StickEvent occurs.
+        Returns the function to be called to unsubscribe later.
+        """
+        return self._manager.subscribe_to_stick_events(
+            stick_event_callback,
+            events,
+        )
+
+    def subscribe_to_node_responses(
+        self,
+        node_response_callback: Callable[[PlugwiseResponse], Awaitable[None]],
+        mac: bytes | None = None,
+        identifiers: tuple[bytes] | None = None,
+    ) -> Callable[[], None]:
+        return self._manager.subscribe_to_node_responses(
+            node_response_callback,
+            mac,
+            identifiers,
+        )
+
+    async def _handle_stick_event(self, event: StickEvent) -> None:
+        """Handle stick events"""
+        if event == StickEvent.CONNECTED:
+            if not self._queue.running:
+                self._queue.start(self._manager)
+                await self.initialize_stick()
+        elif event == StickEvent.DISCONNECTED:
+            if self._queue.running:
+                await self._queue.stop()
+
+    async def initialize_stick(self) -> None:
+        """
+        Initialize connection to the USB-stick.
+        Raises StickError if initialization fails.
+        """
+        if not self._manager.is_connected:
+            raise StickError(
+                "Cannot initialize USB-stick, connected to USB-stick first"
+            )
+        if not self._queue.running:
+            raise StickError("Cannot initialize, queue manager not running")
+
+        try:
+            init_response: StickInitResponse = self._queue.submit(
+                StickInitRequest()
+            )
+        except StickError as err:
+            raise StickError(
+                "No response from USB-Stick to initialization request." +
+                " Validate USB-stick is connected to port " +
+                f"' {self._manager.serial_path}'"
+            ) from err
+        self._mac_stick = init_response.mac_decoded
+        self._network_online = init_response.network_online
+
+        # Replace first 2 characters by 00 for mac of circle+ node
+        self._mac_nc = init_response.mac_network_controller
+        self._network_id = init_response.network_id
+
+    async def send(self, request: PlugwiseRequest) -> PlugwiseResponse:
+        """Submit request to queue and return response"""
+        return await self._queue.submit(request)
+
+    def _reset_states(self) -> None:
+        """Reset internal connection information."""
+        self._mac_stick = None
+        self._mac_nc = None
+        self._network_id = None
+        self._network_online = False
+
+    async def disconnect_from_stick(self) -> None:
+        """Disconnect from USB-Stick."""
+        await self._manager.disconnect_from_stick()
