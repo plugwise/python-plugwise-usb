cfd2dbe4f8fc8dc2c12066401942649692eacd0b
Breugel <frank_van_breugel@hotmail.com>
2024-02-01 21:27:54 +0100

    Use the python 3.10 compatible UTC constant

--- plugwise_usb/nodes/circle_plus.py
+++ plugwise_usb/nodes/circle_plus.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from datetime import datetime, UTC
+from datetime import datetime, timezone
 import logging
 
 from .helpers import raise_not_loaded
@@ -131,7 +131,7 @@ class PlugwiseCirclePlus(PlugwiseCircle):
             minute=clock_response.time.value.minute,
             second=clock_response.time.value.second,
             microsecond=0,
-            tzinfo=UTC,
+            tzinfo=timezone.utc,
         )
         clock_offset = (
             clock_response.timestamp.replace(microsecond=0) - _dt_of_circle
