d251c7e25f61d8ad76f6c7e80ca2c0e99fcd66ea
Frank <frank_van_breugel@hotmail.com>
2025-01-05 21:28:04 +0100

    Update Switch

--- plugwise_usb/nodes/switch.py
+++ plugwise_usb/nodes/switch.py
@@ -2,11 +2,15 @@
 
 from __future__ import annotations
 
-from collections.abc import Callable
+from asyncio import gather
+from collections.abc import Awaitable, Callable
+from datetime import datetime
 import logging
+from typing import Any, Final
 
 from ..api import NodeEvent, NodeFeature
-from ..exceptions import MessageError
+from ..connection import StickController
+from ..exceptions import MessageError, NodeError
 from ..messages.responses import (
     NODE_SWITCH_GROUP_ID,
     NodeSwitchGroupResponse,
@@ -18,29 +22,43 @@ from .helpers.firmware import SWITCH_FIRMWARE_SUPPORT
 
 _LOGGER = logging.getLogger(__name__)
 
+CACHE_SWITCH_STATE: Final = "switch_state"
+CACHE_SWITCH_TIMESTAMP: Final = "switch_timestamp"
+
 
 class PlugwiseSwitch(NodeSED):
     """Plugwise Switch node."""
 
-    _switch_subscription: Callable[[], None] | None = None
-    _switch_state: bool | None = None
+    def __init__(
+        self,
+        mac: str,
+        address: int,
+        controller: StickController,
+        loaded_callback: Callable[[NodeEvent, str], Awaitable[None]],
+    ):
+        """Initialize Scan Device."""
+        super().__init__(mac, address, controller, loaded_callback)
+        self._switch_subscription: Callable[[], None] | None = None
+        self._switch_state: bool | None = None
+        self._switch: bool | None = None
 
     async def load(self) -> bool:
         """Load and activate Switch node features."""
         if self._loaded:
             return True
-        self._node_info.is_battery_powered = True
         if self._cache_enabled:
             _LOGGER.debug("Load Switch node %s from cache", self._node_info.mac)
-            if await self._load_from_cache():
-                self._loaded = True
-                self._setup_protocol(
-                    SWITCH_FIRMWARE_SUPPORT,
-                    (NodeFeature.INFO, NodeFeature.SWITCH),
-                )
-                if await self.initialize():
-                    await self._loaded_callback(NodeEvent.LOADED, self.mac)
-                    return True
+            await self._load_from_cache()
+        else:
+            self._load_defaults()
+        self._loaded = True
+        self._setup_protocol(
+            SWITCH_FIRMWARE_SUPPORT,
+            (NodeFeature.BATTERY, NodeFeature.INFO, NodeFeature.PING, NodeFeature.SWITCH),
+        )
+        if await self.initialize():
+            await self._loaded_callback(NodeEvent.LOADED, self.mac)
+            return True
         _LOGGER.debug("Load of Switch node %s failed", self._node_info.mac)
         return False
 
@@ -49,7 +67,7 @@ class PlugwiseSwitch(NodeSED):
         """Initialize Switch node."""
         if self._initialized:
             return True
-        self._switch_subscription = self._message_subscribe(
+        self._switch_subscription = await self._message_subscribe(
             self._switch_group,
             self._mac_in_bytes,
             (NODE_SWITCH_GROUP_ID,),
@@ -58,10 +76,19 @@ class PlugwiseSwitch(NodeSED):
 
     async def unload(self) -> None:
         """Unload node."""
-        self._loaded = False
         if self._switch_subscription is not None:
             self._switch_subscription()
         await super().unload()
+
+    # region Properties
+
+    @property
+    @raise_not_loaded
+    def switch(self) -> bool:
+        """Current state of switch."""
+        return bool(self._switch_state)
+
+    #endregion
 
     async def _switch_group(self, response: PlugwiseResponse) -> bool:
         """Switch group request from Switch."""
@@ -69,18 +96,67 @@ class PlugwiseSwitch(NodeSED):
             raise MessageError(
                 f"Invalid response message type ({response.__class__.__name__}) received, expected NodeSwitchGroupResponse"
             )
+        await gather(
+            self._available_update_state(True, response.timestamp),
+            self._switch_state_update(response.switch_state, response.timestamp)
+        )
+        return True
+
+    async def _switch_state_update(
+        self, switch_state: bool, timestamp: datetime
+    ) -> None:
+        """Process motion state update."""
+        _LOGGER.debug(
+            "_switch_state_update for %s: %s -> %s",
+            self.name,
+            self._switch_state,
+            switch_state,
+        )
+        state_update = False
         # Switch on
-        if response.switch_state:
+        if switch_state:
+            self._set_cache(CACHE_SWITCH_STATE, "True")
             if self._switch_state is None or not self._switch:
                 self._switch_state = True
-                await self.publish_feature_update_to_subscribers(
-                    NodeFeature.SWITCH, True
-                )
-            return True
-        # Switch off
-        if self._switch is None or self._switch:
-            self._switch = False
-            await self.publish_feature_update_to_subscribers(
-                NodeFeature.SWITCH, False
+                state_update = True
+        else:
+            # Switch off
+            self._set_cache(CACHE_SWITCH_STATE, "False")
+            if self._switch is None or self._switch:
+                self._switch_state = False
+                state_update = True
+        self._set_cache(CACHE_SWITCH_TIMESTAMP, timestamp)
+        if state_update:
+            self._switch = switch_state
+            await gather(
+                *[
+                    self.publish_feature_update_to_subscribers(
+                        NodeFeature.SWITCH, self._switch_state
+                    ),
+                    self.save_cache(),
+                ]
             )
-        return True
+
+    @raise_not_loaded
+    async def get_state(self, features: tuple[NodeFeature]) -> dict[NodeFeature, Any]:
+        """Update latest state for given feature."""
+        states: dict[NodeFeature, Any] = {}
+        for feature in features:
+            _LOGGER.debug(
+                "Updating node %s - feature '%s'",
+                self._node_info.mac,
+                feature,
+            )
+            if feature not in self._features:
+                raise NodeError(
+                    f"Update of feature '{feature.name}' is "
+                    + f"not supported for {self.mac}"
+                )
+            if feature == NodeFeature.SWITCH:
+                states[NodeFeature.SWITCH] = self._switch_state
+            else:
+                state_result = await super().get_state((feature,))
+                states[feature] = state_result[feature]
+        if NodeFeature.AVAILABLE not in states:
+            states[NodeFeature.AVAILABLE] = self.available_state
+        return states
