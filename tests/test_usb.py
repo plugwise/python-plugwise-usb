1f8e7e64a85e0c0f1d2231d1cff18fe0ed0d3cad
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-02-07 19:41:18 +0100

    Add stick-hw-fw asserts

--- tests/test_usb.py
+++ tests/test_usb.py
@@ -460,8 +460,10 @@ class TestStick:
         await stick.initialize()
         assert stick.mac_stick == "0123456789012345"
         assert stick.mac_coordinator == "0098765432101234"
-        _LOGGER.debug("HOI fw_stick: %s", stick.firmware_stick)
-        _LOGGER.debug("HOI hw_stick: %s", stick.hardware_stick)
+        assert stick.firmware_stick == dt(
+            2011, 6, 27, 8, 47, 37, tzinfo=UTC
+        )
+        assert stick.hardware_stick == "000000730007"
         assert not stick.network_discovered
         assert stick.network_state
         assert stick.network_id == 17185
