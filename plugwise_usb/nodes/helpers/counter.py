d87c8c7370f7749a3b859b20e40067ed78998755
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-03-03 08:03:18 +0100

    Handle negative energy-pulse values

--- plugwise_usb/nodes/helpers/counter.py
+++ plugwise_usb/nodes/helpers/counter.py
@@ -261,7 +261,12 @@ class EnergyCounter:
             return None
         if self._pulses == 0:
             return 0.0
-        pulses_per_s = self._pulses / float(HOUR_IN_SECONDS)
+        # Handle both positive and negative pulses values
+        negative = False
+        if self._pulses < 0:
+            negative = True
+
+        pulses_per_s = abs(self._pulses) / float(HOUR_IN_SECONDS)
         corrected_pulses = HOUR_IN_SECONDS * (
             (
                 (
@@ -276,8 +281,8 @@ class EnergyCounter:
             + self._calibration.off_tot
         )
         calc_value = corrected_pulses / PULSES_PER_KW_SECOND / HOUR_IN_SECONDS
-        # Guard for minor negative miscalculations
-        calc_value = max(calc_value, 0.0)
+        if negative:
+            calc_value = -calc_value
         return calc_value
 
     @property
