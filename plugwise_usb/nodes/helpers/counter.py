944e0e3f7c26fa364c5282625d4c36bb5bb6fd79
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-03-13 20:08:10 +0100

    Fix typo in logging

--- plugwise_usb/nodes/helpers/counter.py
+++ plugwise_usb/nodes/helpers/counter.py
@@ -324,5 +324,5 @@ class EnergyCounter:
         self._pulses = pulses
 
         energy = self.energy
-        _LOGGER.debug("energy=%s or last_update=%s", energy, last_update)
+        _LOGGER.debug("energy=%s on last_update=%s", energy, last_update)
         return (energy, last_reset)
