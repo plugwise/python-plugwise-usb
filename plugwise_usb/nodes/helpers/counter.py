e5d78af1f3137fdd81bb293fa33b9790020f58c2
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-03-27 15:20:39 +0100

    Counter.py: clean up imports

--- plugwise_usb/nodes/helpers/counter.py
+++ plugwise_usb/nodes/helpers/counter.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from enum import Enum, auto
 import logging
 from typing import Final
