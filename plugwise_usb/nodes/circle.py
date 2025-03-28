28622191d818a6c81b975d6571fd254303717304
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-03-28 10:38:19 +0100

    Try to not block the event_loop

--- plugwise_usb/nodes/circle.py
+++ plugwise_usb/nodes/circle.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from asyncio import Task, create_task
+from asyncio import Task, create_task, gather
 from collections.abc import Awaitable, Callable
 from dataclasses import replace
 from datetime import UTC, datetime
@@ -449,9 +449,12 @@ class PlugwiseCircle(PlugwiseBaseNode):
             total_addresses = 11
             log_address = self._current_log_address
             while total_addresses > 0:
-                await self.energy_log_update(log_address)
+                task = create_task(self.energy_log_update(log_address))
+                await task
                 log_address, _ = calc_log_address(log_address, 1, -4)
                 total_addresses -= 1
+
+            await gather(*task)
 
             if self._cache_enabled:
                 await self._energy_log_records_save_to_cache()
@@ -472,7 +475,10 @@ class PlugwiseCircle(PlugwiseBaseNode):
 
             missing_addresses = sorted(missing_addresses, reverse=True)
             for address in missing_addresses:
-                await self.energy_log_update(address)
+                task = create_task(self.energy_log_update(address))
+                await task
+
+            gather(*task)
 
         if self._cache_enabled:
             await self._energy_log_records_save_to_cache()
