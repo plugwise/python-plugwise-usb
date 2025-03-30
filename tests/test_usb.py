b76827b5367565b7da63411fba386edbbd6b6385
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-03-30 11:27:35 +0200

    Test: line up 2nd test-time

--- tests/test_usb.py
+++ tests/test_usb.py
@@ -1116,7 +1116,7 @@ class TestStick:
         tst_consumption.update_pulse_counter(45, 0, pulse_update_3)
         assert tst_consumption.log_rollover
         assert tst_consumption.hourly_reset_time == pulse_update_3
-        test_timestamp = fixed_this_hour + td(hours=1, seconds=5)
+        test_timestamp = fixed_this_hour + td(hours=1, minutes=1, seconds=5)
         assert tst_consumption.collected_pulses(
             test_timestamp, is_consumption=True
         ) == (45, pulse_update_3)
