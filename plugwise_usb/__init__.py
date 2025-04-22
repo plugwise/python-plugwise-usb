ef84f9877c5af57c5097bbb0129d1886c262fe39
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-04-22 14:51:20 +0200

    Try async setter construct

--- plugwise_usb/__init__.py
+++ plugwise_usb/__init__.py
@@ -191,7 +191,7 @@ class Stick:
         return self._network.accept_join_request
 
     @accept_join_request.setter
-    def accept_join_request(self, state: bool) -> None:
+    async def accept_join_request(self, state: bool) -> None:
         """Configure join request setting."""
         if not self._controller.is_connected:
             raise StickError(
@@ -206,7 +206,7 @@ class Stick:
             )
 
         self._network.accept_join_request = state
-        task = create_task(self._network.allow_join_requests(state))
+        await self._network.allow_join_requests(state)
 
     async def clear_cache(self) -> None:
         """Clear current cache."""
