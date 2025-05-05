fa7549853d2ff66ed2a87f209f1837428a3e13fe
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-05-05 12:20:58 +0200

    Re-fix guarding

--- plugwise_usb/network/__init__.py
+++ plugwise_usb/network/__init__.py
@@ -244,7 +244,7 @@ class StickNetwork:
                 f"Invalid response message type ({response.__class__.__name__}) received, expected NodeJoinAvailableResponse"
             )
         mac = response.mac_decoded
-        if self.accept_join_request:
+        if self.accept_join_request and await self.register_node(mac):
             await self._notify_node_event_subscribers(NodeEvent.JOIN, mac)
             return True
         
