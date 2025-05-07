3d488b9f63cfd86f857883bef02cd73835100f15
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-05-07 09:03:22 +0200

    Create NodeResponseType REJOINING and use

--- plugwise_usb/network/registry.py
+++ plugwise_usb/network/registry.py
@@ -250,8 +250,9 @@ class StickNetworkRegister:
 
         request = NodeAddRequest(self._send_to_controller, bytes(mac, UTF8), True)
         try:
-            if (response := await request.send()) is None:  # or response.ack_id != NodeResponseType.JOIN_ACCEPTED:
-                raise NodeError(f"Failed to register node {mac}")
+            response = await request.send()
+            if response is None or response.response_type != NodeResponseType.REJOINING:
+                raise NodeError(f"Failed to register node {mac}, no or wrong response")
         except MessageError as exc:
             raise MessageError(f"Failed to register Node with {mac}") from exc
 
