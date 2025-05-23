e946297cc42c064b0a34bf0ed67d4ef3f32ff7c9
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-05-23 16:18:56 +0200

    Use negative to avoid pass

--- plugwise_usb/nodes/celsius.py
+++ plugwise_usb/nodes/celsius.py
@@ -27,14 +27,14 @@ class PlugwiseCelsius(NodeSED):
         """Load and activate node features."""
         if self._loaded:
             return True
-        self._node_info.is_battery_powered = True
 
+        self._node_info.is_battery_powered = True
         if self._cache_enabled:
             _LOGGER.debug(
                 "Load Celsius node %s from cache", self._node_info.mac
             )
-            if await self._load_from_cache():
-                pass
+            if not await self._load_from_cache():
+                _LOGGER.debug("Loading from cache failed")
 
         self._loaded = True
         self._setup_protocol(
@@ -44,5 +44,6 @@ class PlugwiseCelsius(NodeSED):
         if await self.initialize():
             await self._loaded_callback(NodeEvent.LOADED, self.mac)
             return True
+
         _LOGGER.debug("Load of Celsius node %s failed", self._node_info.mac)
         return False
