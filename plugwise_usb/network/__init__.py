dc5f4a966c3068e9126322cf315b89fab7eaa2d5
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-06-11 15:59:12 +0200

    Start adding _current_log_address reset

--- plugwise_usb/network/__init__.py
+++ plugwise_usb/network/__init__.py
@@ -14,6 +14,7 @@ from ..api import NodeEvent, NodeType, PlugwiseNode, StickEvent
 from ..connection import StickController
 from ..constants import ENERGY_NODE_TYPES, UTF8
 from ..exceptions import CacheError, MessageError, NodeError, StickError, StickTimeout
+from ..nodes.circle import PlugwiseCircle
 from ..nodes.helpers.cache import NodeCache
 from ..nodes.helpers.pulses import PulseCollection
 from ..helpers.util import validate_mac
@@ -551,9 +552,13 @@ class StickNetwork:
             node_cache.update_state(CACHE_ENERGY_COLLECTION, "")
             await node_cache.save_cache()
 
-        # Clear PulseCollection._logs
+        # Reset the PulseCollection
         pulse_collection = PulseCollection(mac)
         pulse_collection.reset()
+
+        # Clear PlugwiseCircle._current_log_address
+        circle = PlugwiseCircle(mac, address(?))
+        circle._current_log_address = 0
 
     async def set_energy_intervals(
         self, mac: str, consumption: int, production: int
