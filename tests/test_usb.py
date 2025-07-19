076ff5bd188f2daad4b36386ac1dc18baece2d53
Marc Dirix <marc@dirix.nu>
2025-07-19 15:28:53 +0200

    await _load_defaults

--- tests/test_usb.py
+++ tests/test_usb.py
@@ -2177,7 +2177,7 @@ class TestStick:
             assert await test_scan.set_motion_reset_timer(0)
         with pytest.raises(ValueError):
             assert await test_scan.set_motion_reset_timer(256)
-        assert await test_scan.set_motion_reset_timer(10)
+        assert not await test_scan.set_motion_reset_timer(10)
         assert test_scan.scan_config_task_scheduled
         assert await test_scan.set_motion_reset_timer(15)
         assert test_scan.scan_config_task_scheduled
