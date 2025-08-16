985d56c24511d41285a5ea200f1912eb5f66b973
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-08-16 11:57:04 +0200

    Catch TypeError in validate_mac()

--- plugwise_usb/helpers/util.py
+++ plugwise_usb/helpers/util.py
@@ -7,14 +7,19 @@ import re
 import crcmod
 
 from ..constants import HW_MODELS
+from ..exceptions import NodeError
 
 crc_fun = crcmod.mkCrcFun(0x11021, rev=False, initCrc=0x0000, xorOut=0x0000)
 
 
 def validate_mac(mac: str) -> bool:
     """Validate the supplied string is in a MAC address format."""
-    if not re.match("^[A-F0-9]+$", mac):
+    try:
+        if not re.match("^[A-F0-9]+$", mac):
+            return False
+    except TypeError:
         return False
+       
     try:
         _ = int(mac, 16)
     except ValueError:
