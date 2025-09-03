f1abce90950bf953efed75754783bb6135386b98
Marc Dirix <marc@dirix.nu>
2025-09-03 10:31:33 +0200

    fix naming 2.0

--- plugwise_usb/messages/requests.py
+++ plugwise_usb/messages/requests.py
@@ -1428,7 +1428,7 @@ class SenseConfigureHysteresisRequest(PlugwiseRequest):
         upper_bound: int,
         direction: bool,
     ):
-        """Initialize ScanConfigureHysteresisRequest message object."""
+        """Initialize SenseConfigureHysteresisRequest message object."""
         super().__init__(send_fn, mac)
         temp_hum_value = Int(1 if temp_hum else 0, length=2)
         lower_bound_value = Int(lower_bound, length=4)
