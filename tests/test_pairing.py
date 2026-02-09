7be4d41851f79002726e5220fcf0ea40b9da0590
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-02-09 08:46:23 +0100

    Don't test network down first

--- tests/test_pairing.py
+++ tests/test_pairing.py
@@ -52,10 +52,10 @@ RESPONSE_MESSAGES = {
         + b"0123456789012345"  # stick-mac
         + b"0F"  # channel
         + b"FFFFFFFFFFFFFFFF"
-        + b"0698765432101234"  # 06 + plus-device mac
+        + b"FF98765432101234"  # 06 + plus-device mac
         + b"FFFFFFFFFFFFFFFF"
-        + b"0698765432101234"  # 06 + plus-device mac
-        + b"1606"  # pan_id
+        + b"FF98765432101234"  # 06 + plus-device mac
+        + b"04FF"  # pan_id
         + b"01",  # index
     ),
     b"\x05\x05\x03\x03000AB43C\r\n": (
@@ -64,7 +64,10 @@ RESPONSE_MESSAGES = {
         b"0011"  # msg_id
         + b"0123456789012345"  # stick mac
         + b"00"  # unknown1
-        + b"00",  # network_is_offline
+        + b"01"  # network_is_online
+        + b"FF98765432101234"
+        + b"04FF"
+        + b"FF",
     ),
     b"\x05\x05\x03\x0300040000000000000000000098765432101234\r\n": (
         "Pair request of plus-device 0098765432101234",
@@ -414,54 +417,6 @@ class TestStick:
     #            await stick.initialize()
     #        await stick.disconnect()
 
-    RESPONSE_MESSAGES = {
-        b"\x05\x05\x03\x030001CAAB\r\n": (
-            "Stick network info request",
-            b"000000C1",  # Success ack
-            b"0002"  # response msg_id
-            + b"0123456789012345"  # stick-mac
-            + b"0F"  # channel
-            + b"FFFFFFFFFFFFFFFF"
-            + b"FF98765432101234"  # 06 + plus-device mac
-            + b"FFFFFFFFFFFFFFFF"
-            + b"FF98765432101234"  # 06 + plus-device mac
-            + b"04FF"  # pan_id
-            + b"01",  # index
-        ),
-        b"\x05\x05\x03\x03000AB43C\r\n": (
-            "STICK INIT",
-            b"000000C1",  # Success ack
-            b"0011"  # msg_id
-            + b"0123456789012345"  # stick mac
-            + b"00"  # unknown1
-            + b"01"  # network_is_online
-            + b"FF98765432101234"
-            + b"04FF"
-            + b"FF",
-        ),
-        b"\x05\x05\x03\x0300040000000000000000000098765432101234\r\n": (
-            "Pair request of plus-device 0098765432101234",
-            b"000000C1",  # Success ack
-            b"0005"  # response msg_id
-            + b"00"  # existing
-            + b"01",  # allowed
-        ),
-        b"\x05\x05\x03\x0300230123456789012345A0EC\r\n": (
-            "Node Info of stick 0123456789012345",
-            b"000000C1",  # Success ack
-            b"0024"  # msg_id
-            + b"0123456789012345"  # mac
-            + b"00000000"  # datetime
-            + b"00000000"  # log address 0
-            + b"00"  # relay
-            + b"80"  # hz
-            + b"653907008512"  # hw_ver
-            + b"4E0843A9"  # fw_ver
-            + b"00",  # node_type (Stick)
-        ),
-    }
-
-
     @pytest.mark.asyncio
     async def test_pair_plus(self, monkeypatch: pytest.MonkeyPatch) -> None:
         """Test pairing a plus-device."""
@@ -474,11 +429,9 @@ class TestStick:
         monkeypatch.setattr(pw_sender, "STICK_TIME_OUT", 0.1)
         monkeypatch.setattr(pw_requests, "NODE_TIME_OUT", 0.5)
         stick = pw_stick.Stick(port="test_port", cache_enabled=False)
-        await stick.connect("test_port")
-        with pytest.raises(pw_exceptions.StickError):
-            await stick.initialize()
-
-        await asyncio.sleep(5)
+        # await stick.connect("test_port")
+        # with pytest.raises(pw_exceptions.StickError):
+        #     await stick.initialize()
         await stick.plus_pair_request("0098765432101234")
         await asyncio.sleep(5)
 
