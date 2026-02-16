11a9716197596961fd1ab71a7ff7e0733da65bfc
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-02-16 18:22:04 +0100

    Improve

--- plugwise_usb/messages/requests.py
+++ plugwise_usb/messages/requests.py
@@ -534,9 +534,7 @@ class StickInitRequest(PlugwiseRequest):
         if self._send_fn is None:
             raise MessageError("Send function missing")
         result = await self._send_request()
-        if isinstance(result, StickInitResponse) or isinstance(
-            result, StickInitShortResponse
-        ):
+        if isinstance(result, (StickInitResponse, StickInitShortResponse)):
             return result
         if result is None:
             return None
